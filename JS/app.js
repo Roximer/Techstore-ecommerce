a642a6fdc8b32b08fe0fad987eebbbe133838df4
Alejandra Alaniz <dominguezale377@gmail.com>
2023-09-11 23:32:24 -0300

    changes in navbar.js

--- JS/app.js
+++ JS/app.js
@@ -15,7 +15,7 @@ let productos= JSON.parse(localStorage.getItem("productos")) || null;
         <div class="card-body">
         <h5 class=" card-title">${producto.nombre}</h5>   
         <p class= "car-text"> </p>
-        <a href="#" class="btn d-grid btn-primary">Ver más</a>
+        <a href="#" class="btn d-grid  btn-primary">Ver más</a>
         </div>
         </div>` ;  
         col.innerHTML =tarjeta;
